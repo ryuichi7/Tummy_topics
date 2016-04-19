d50a94502ce9dd760547a67cd61153afc6a90986
Brett <brettrheenan@gmail.com>
2016-04-19 12:37:32 +0200

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,40 @@
 end
 
 
-10.times do
+30.times do
 	Ingredient.create(
 		name: Faker::Commerce.product_name
 		)
 end
+
+30.times do
+	Recipe.create(
+		name: Faker::Lorem.word,
+		description: Faker::Lorem.paragraph,
+		directions: Faker::Lorem.paragraph,
+		user: User.find(rand(1..10)),
+		ingredients_attributes: [*1..4].collect { Ingredient.find(rand(1..30)) }
+	)
+end
+
+30.times do 
+	Comment.create(
+		content: Faker::Lorem.paragraph,
+		commenter: User.find(rand(1..10)),
+		recipe: Recipe.find(rand(1..30))
+		)
+end
+
+30.times do 
+	Rating.create(
+		score: rand(1..5),
+		rater: User.find(rand(1..10)),
+		recipe: Recipe.find(rand(1..30))
+	)
+end
+
+Recipe.all.each do |recipe|
+	recipe.comments << [*1..4].collect { Comment.find(rand(1..30)) }
+	recipe.ratings << [*1..4].collect { Rating.find(rand(1..30)) }
+end
+
